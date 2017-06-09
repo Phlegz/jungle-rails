47f0e2ddbe1655539e8aa87e9e8dad8c32b0c1ae
farnaz <farnaz.rashidi@gmail.com>
2017-06-09 15:28:47 -0700

    Display the review in the product page

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ActiveRecord::Base
   mount_uploader :image, ProductImageUploader
 
   belongs_to :category
+  has_many :review, dependent: :destroy
 
   validates :name, presence: true
   validates :price, presence: true
